6344583e76765f1da0c269ff2d85c0c7ff06adc9
Rafael Garcia <rafbgarcia@gmail.com>
2018-08-25 21:04:46 -0300

    Export utils

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import Money from './component'
 import VMoney from './directive'
 import options from './options'
+import * as utils from './utils'
 const VERSION = proccess.env.VERSION
 
 export {
   Money,
   VMoney,
   options,
+  utils,
   VERSION
 }
 
